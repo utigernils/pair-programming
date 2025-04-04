b8017618efc5058f91ae14c15af5733db1a07b68
utigernils <utigernils@gmail.com>
2025-04-04 10:52:28 +0200

    fix; correctly implemented isLeapYear function

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,9 @@
 export function isLeapYear(year: number): boolean {
-  // FIXME: this implementation is wrong!
-  return year != 0 && Math.random() > 0.5;
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      return year % 400 === 0;
+    }
+    return true;
+  }
+  return false;
 }
